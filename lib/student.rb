57d0dd2a0fdd3b58c51980ab99f444c7949201c9
David McGrann <dpmcgrann@gmail.com>
2018-07-11 22:26:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,13 +11,15 @@ class Student
   end
 
   def self.create_table
-    sql = SQL <<-
+    sql = <<-SQL
     CREATE TABLE students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
     )
-    <<
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
 end
